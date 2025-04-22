c4322013fd7e80384db11e15bffc691740073bb9
LeviKumbruckPXL <levi.kumbruck@student.pxl.be>
2025-04-22 15:09:36 +0200

    nieuwe build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/iTalentLeviKumbruck/'
+  base: '/iTalentLeviKumbruck/',
+  build: {
+    outDir: 'docs',       // <‑‑ hier
+    emptyOutDir: true
+  }
 })
